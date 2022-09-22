7e844c6811af270dfbdd9ed02069d4c04f24dc7b
= <orhanbayram@outlook.com>
2022-09-22 23:10:06 +0300

    brand links updated

--- components/BrandList.vue
+++ components/BrandList.vue
@@ -3,50 +3,62 @@ const brands = [
   {
     name: "Vue JS",
     logo: "/img/vue.png",
+    link: "https://vuejs.org/",
   },
   {
     name: "Nuxt JS",
     logo: "/img/nuxt.png",
+    link: "https://nuxtjs.org/",
   },
   {
     name: "React JS",
     logo: "/img/react.png",
+    link: "https://reactjs.org/",
   },
   {
     name: "Vite",
     logo: "/img/vite.png",
+    link: "https://vitejs.dev/",
   },
   {
     name: "Node JS",
     logo: "/img/nodejs.png",
+    link: "https://nodejs.org/en/",
   },
   {
     name: "Tailwind CSS",
     logo: "/img/tailwind.png",
+    link: "https://tailwindcss.com/",
   },
   {
     name: "SASS",
     logo: "/img/sass.png",
+    link: "https://sass-lang.com/",
   },
   {
     name: "Adobe XD",
     logo: "/img/adobexd.png",
+    link: "https://www.adobe.com/tr/products/xd.html",
   },
   {
     name: "HTML5",
     logo: "/img/html5.png",
+    link: "https://tr.wikipedia.org/wiki/HTML5",
   },
   {
     name: "GSAP",
     logo: "/img/gsap.svg",
+    link: "https://greensock.com/gsap/",
   },
   {
     name: "Supabase",
     logo: "/img/supabase.png",
+    link: "https://supabase.com/",
   },
   {
     name: "Docker",
     logo: "/img/docker.webp",
+    link: "https://www.docker.com/",
   },
 ];
 </script>
@@ -56,7 +68,7 @@ const brands = [
     <a
       v-for="brand in brands"
       :key="brand.name"
-      href="#"
+      :href="brand.link"
       :title="brand.name"
       class="p-4 hover:bg-gray-800 rounded"
       target="_blank"
